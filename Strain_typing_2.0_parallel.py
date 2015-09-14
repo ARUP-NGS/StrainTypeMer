fixed histogram calculation

Strain_typing_2.0_parallel.py
@@ -9,11 +9,9 @@ The input of strains should be provided should be provided as a comma separated 
 assume sample name is before underscore
 """
 import jf_object as jfobj
-
-# import jellyfish
 import numpy as np
 from numpy import sum
-
+import math
 import argparse
 import os.path
 import sys
@@ -22,10 +20,18 @@ import collections
 from multiprocessing import Process, Queue
 import string
 import random
-# import itertools
+
+## CONFIGURATION INFORMATION ###########################################################################################
+jellyfish_path = "/home/ksimmon/bin/jellyfish-2.2.0/bin/jellyfish"
+jfobj.jellyfish_path = jellyfish_path
+jfobj.ardb_dir = "/home/ksimmon/reference/strian_typing_resources/ARDB/grouped_fastas/jf_files/dump/"
+jfobj.ardb_info = "/home/ksimmon/reference/strian_typing_resources/ARDB/class2info.tab"
+#######################################################################################################
+
+def output_histo(jf_objects, xlim=350000, ylim=150):
+    pass
 
 
-jellyfish_path = "/home/ksimmon/bin/Jellyfish/bin/jellyfish"
 
 def count_kmers(q, merged_jf_obj, this_jf_object, cutoff):
     """
@@ -48,6 +54,9 @@ def main():
     parser = argparse.ArgumentParser(description="the script take multiple jellyfish counts and compares the strains")
     parser.add_argument("-c", "--cutoff", help="The number of kmers to analyze [Default = None]", type=int,
               default=None)
+    parser.add_argument("--coverage_cutoff", help="percent of genome coverage to set kmer filters [DEFAULT .20 if "  + \
+                                                  "coverage is 30 [(30 * .20) = 6] kmers with a count < 5 will " + \
+                                                  "be ignored for cooresponding strain", type=float, default=.20)
     parser.add_argument("-t", "--cpus",
               help="The number of cpus to use when counting kmers in strains [Default is the len of the strain list]",
               type=int, default=2)
@@ -62,21 +71,28 @@ def main():
     no_kmer_filtering = args.no_kmer_filtering
     cutoff = args.cutoff
     cpus = args.cpus
+    coverage_cutoff = args.coverage_cutoff
     jf_files = args.jf_files
     kmer_reference = args.kmer_reference
+
+
+    compare_strains(jf_files=jf_files, no_kmer_filtering=no_kmer_filtering, cutoff=cutoff, cpus=cpus, coverage_cutoff=coverage_cutoff,kmer_reference=kmer_reference)
     ####################################################################################################################
     # CHECK ARGUMENTS AND ATTRIBUTES
     ####################################################################################################################
+
+def compare_strains(jf_files, no_kmer_filtering, cutoff, cpus, coverage_cutoff, kmer_reference):
     if kmer_reference is not None and os.path.isfile(kmer_reference) is False:
         sys.stderr.write("kmer reference file does not exist: {0}\n".format(kmer_reference))
         sys.exit(11)
 
     NUM_OF_STRAINS = len(jf_files) #calculate the number of samples
-    if cpus == None:
+    if cpus == None or cpus > NUM_OF_STRAINS:
         cpus = NUM_OF_STRAINS
 
-    file_paths = []
 
+
+    file_paths = []
     strain_objs = collections.OrderedDict()  ##KEEP RECORD OF FILE PATHS and CREATE THE STRAIN OBJS FOR EACH STRAIN
     for f in jf_files:
         if os.path.exists(f) == False: ##MAKE SURE THE FILE EXISTS
@@ -107,8 +123,7 @@ def main():
         # dump kmers out to a file [Technically I should be able to remove this however a jellyfish bug causes the loss
         # of canonicalization for merged sets ] dumping out to txt file avoids this.
         try:
-            subprocess.check_call([jellyfish_path,
-                                   "dump", '-c',  "-t", "-o", dump_temp_file, jf_temp_file] )
+            subprocess.check_call([jellyfish_path, "dump", '-c',  "-t", "-o", dump_temp_file, jf_temp_file] )
         except:
             sys.stderr.write("Error in running jellyfish merge\n")
             sys.exit(4)
@@ -127,6 +142,7 @@ def main():
     ########################################################################################################################
     #START THE WORK
     ########################################################################################################################
+    ardb_results = {}
     count_table = [[] for i in range(len(strain_objs)) ]
     counter = 0
     attach = count_table.append
@@ -137,6 +153,8 @@ def main():
     jobs = strain_objs.values()
     num_of_strains_counted = 0
     current_processes = []
+
+
     for cpu in range(cpus):
         _obj = jobs.pop()
         p = Process(target=count_kmers, args=(q, merged_jf, _obj, cutoff,), name=_obj.name)
@@ -148,9 +166,13 @@ def main():
 
     #keep the queue moving
     while len(jobs) != 0:
-        _name, _arr = q.get() #PAUSES UNTIL A JOBS RETURN  #AM I WIATING FOR THE FIRST OBJECT
+        _name, _arr, ardb = q.get() #PAUSES UNTIL A JOBS RETURN  #AM I WIATING FOR THE FIRST OBJECT
+        ardb_results.update({_name : ardb})
         num_of_strains_counted += 1
+
         sys.stderr.write("{0}\tof\t{1}\tstrains processed\n".format(num_of_strains_counted, len(strain_objs)))
+
+
         count_table[strain_objs.keys().index(_name)]=np.array(_arr)
         #start next job
         p = Process(target=count_kmers, args=( q, merged_jf, jobs.pop(), cutoff, ),name=_obj.name)
@@ -159,8 +181,10 @@ def main():
 
     #wait until the queue returns 'ALL THE THINGS'
     while num_of_strains_counted != len(strain_objs): ##finished processing
-        _name, _arr = q.get() #PAUSES UNTIL A JOBS RETURN
+        _name, _arr, ardb = q.get() #PAUSES UNTIL A JOBS RETURN
+        ardb_results.update({_name : ardb})
         num_of_strains_counted += 1
+
         sys.stderr.write("{0}\tof\t{1}\tstrains processed\n".format(num_of_strains_counted, len(strain_objs)))
         count_table[strain_objs.keys().index(_name)]=np.array(_arr)
     q.close()
@@ -188,14 +212,14 @@ def main():
     else:
         for k, v in strain_objs.iteritems():
             sys.stdout.write("Strain: {:s}\t Coverage Estimate: {:.1f}\n".format(k, v.coverage))
-            cutoff_table.append(int(v.coverage * .25) + 1)
-            v.set_cutoff(int(v.coverage * .25) + 1)
+            cutoff_table.append(math.ceil(v.coverage * coverage_cutoff))
+            v.set_cutoff(math.ceil(v.coverage * coverage_cutoff))
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     sys.stdout.write(" STRAIN STATS ".center(80, "-") + "\n")
     for k, v in strain_objs.iteritems():
         sys.stdout.write("Strain: {:s}\n".format(k))
-        sys.stdout.write("\tInferred genome size: {:.0f}  [filtering kmers counted <= than {:.0f} times]\n".format(
+        sys.stdout.write("\tInferred genome size: {:.0f}  [filtering kmers counted <= {:.0f} times]\n".format(
                           v.estimate_genome_size(v.kmer_cutoff), v.kmer_cutoff ))
     sys.stdout.write("".center(80, "-") + "\n\n")
 
@@ -242,13 +266,14 @@ def main():
              similarity_dict[strain_keys[i]].update({
                                                      strain_keys[j] :
                                                          (float(intersection) / total_kmers * 100,
-                                                          float(intersection) / smallest * 100,
+                                                          float(intersection) / total_kmers * 100,
                                                           total_kmers,
                                                           smallest,
                                                           )
                                                     })
 
     #PRINT SIMILARITY TABlE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    sys.stdout.write("[SIMILARITY TABLE]\n")
     delimeter = ","
     _str = delimeter + delimeter.join(strain_keys) + "\n"
     for i in range(len(strain_keys)):
@@ -261,10 +286,11 @@ def main():
             elif i > j:
                 _str += "{:.1f}{:s}".format(similarity_dict[strain_keys[j]][strain_keys[i]][1], delimeter)
         _str  = _str[:-1] + "\n"
-    sys.stdout.write(_str + "\n")
-
+    sys.stdout.write(_str)
+    sys.stdout.write("[SIMILARITY TABLE END]\n")
     #ADD DENOMINATOR TO OUTPUT
     sys.stdout.write("\n\n")
+    sys.stdout.write("[DENOMINATOR TABLE]\n")
     _str = delimeter + delimeter.join(strain_keys) + "\n"
     for i in range(len(strain_keys)):
         _str += strain_keys[i] + delimeter
@@ -276,8 +302,18 @@ def main():
             elif i > j:
                 _str += "{:.1f}{:s}".format(similarity_dict[strain_keys[j]][strain_keys[i]][3], delimeter)
         _str  = _str[:-1] + "\n"
-    sys.stdout.write(_str + "\n")
+    sys.stdout.write(_str)
+    sys.stdout.write("[DENOMINATOR TABLE END]\n")
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #print out ardb
+    sys.stdout.write("[ARDB INFORMATION]\n")
+    for strain, _obj in strain_objs.iteritems():
+        sys.stdout.write("STRAIN_ID:\t{0}\n".format(strain))
+        for k in sorted(ardb_results[strain], key=ardb_results[strain].get, reverse=True) :
+            sys.stdout.write("ARDB_CODE:\t{0}\n\tKMER_COUNT:\t{1}\n\tARDB_INFO:\t{2}\n".format(
+                k, ardb_results[strain][k], _obj.get_ardb_info(k)))
+    sys.stdout.write("[ARDB INFORMATION END]\n")
+
 
 if __name__ == "__main__":
     main()

jf_object.py
@@ -4,28 +4,70 @@ import jellyfish
 import subprocess
 import sys
 import numpy as np
-import multiprocessing
+import os, os.path
+from collections import OrderedDict
 
 class jf_object:
-    """contains information about the jellyfish db for a single strain"""
+    """
+    Contains information about the jellyfish db for a single strain
+    """
     kmer_cutoff = 0
+    #jellyfish_path = ""# = "/home/ksimmon/bin/jellyfish-2.2.0/bin/jellyfish"
+    #ardb_dir = "" # "/home/ksimmon/reference/strian_typing_resources/ARDB/grouped_fastas/jf_files/dump/"
+    #ardb_info = ""
     jellyfish_path = "/home/ksimmon/bin/jellyfish-2.2.0/bin/jellyfish"
+    #jellyfish_path = jellyfish_path
+    ardb_dir = "/home/ksimmon/reference/strian_typing_resources/ARDB/grouped_fastas/jf_files/dump/"
+    ardb_info = "/home/ksimmon/reference/strian_typing_resources/ARDB/class2info.tab"
+
 
     def __init__(self, name, path):
+        """
+        initialize a jelly
+
+        :param name:
+        :param path:
+        :return:
+        """
+        self.jellyfish_path = "/home/ksimmon/bin/jellyfish-2.2.0/bin/jellyfish"
+        self.ardb_dir = "/home/ksimmon/reference/strian_typing_resources/ARDB/grouped_fastas/jf_files/dump/"
+        self.ardb_info = "/home/ksimmon/reference/strian_typing_resources/ARDB/class2info.tab"
+
+
         self.name = name
         self.path = path
         self.qf = jellyfish.QueryMerFile(path)
 
-        self.get_stats()
-        self.get_histo()
-        self.get_estimate_coverage()
 
+
+        self.ardb_info_parsed = self.__parser_ardb_info()
+        self.get_stats()
+        self.histo = self.get_histo()
+        self.get_estimate_coverage()
+        self.__check_resources()
         self.shared_count = None
+
+
+
         #self.percentage_of_kmers()
 
+    def __check_resources(self):
+        if os.path.exists(self.jellyfish_path) is False:
+            sys.stderr.write("Jellyfish path not set\n")
+            sys.stderr.write(self.jellyfish_path + "\n")
+            sys.exit("2")
+        if os.path.isdir(self.ardb_dir) is False:
+            sys.stderr.write("ARDB fasta path not set\n")
+            sys.exit("2")
+        if os.path.isfile(self.ardb_info)is False:
+            sys.stderr.write("ARDB info path not set\n")
+            sys.exit("2")
+
+    def __parser_ardb_info(self):
+         return {line.split("\t")[0]: line.split("\t")[1].strip() for line in open(self.ardb_info,"r")}
 
     def get_stats(self):
-        op = subprocess.Popen(["/home/ksimmon/bin/jellyfish-2.2.0/bin/jellyfish", "stats", self.path],
+        op = subprocess.Popen([self.jellyfish_path, "stats", self.path],
                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = op.communicate()
 
@@ -43,32 +85,41 @@ class jf_object:
 
     def get_histo(self):
         #print self.max_count
-        op = subprocess.Popen(["/home/ksimmon/bin/jellyfish-2.2.0/bin/jellyfish", "histo", "--threads", "4", "--full", "--high", str(self.max_count ),
-                              "--low", "0", self.path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        _histo = OrderedDict()
+        #_histo = { i+1 : 0  for i in range(300)}
+        op = subprocess.Popen([self.jellyfish_path, "histo", self.path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = op.communicate()
 
         if err != "":
             sys.stderr.write("Failed to retrieve db histo\n JELLYFISH ERROR: {0}\n".format(err))
             sys.exit(1)
-        _arr = []
-        out = out.strip().split("\n")
-        self.histo = np.array([ int(i.split(" ")[1])  for i in out])
-        self.histo_total = np.array([(i + 1) * self.histo[i] for i in range(len(self.histo))])
+
+        for v in out.strip().split("\n"):
+            freq, count = [int(i) for i in v.split(" ")]
+            if freq in _histo:
+                _histo[freq] += count
+            else: #greater than 300
+                _histo.update({freq : count })
+        return _histo
+
 
 
     def get_estimate_coverage(self):
         """Estimates the coverage for kmers count > 3 times"""
         try:
-            self.coverage = float(np.sum(self.histo_total[3:])) / float(np.sum(self.histo[3:]))
+            #sum of kmers count / the kmers (distinict) count
+            self.coverage = np.sum([float(i) * self.histo[i] for i in self.histo]) / float(np.sum(self.histo.values()[3:]))
         except:
             self.coverage = 0
 
-    def get_histo_plot(self):
-        pass
-        #TODO
 
-    def estimate_genome_size(self, coverage_cutoff):  ####GET THE FREQUENCY OF KMERS REPEATED IN ACINETO GENOME
-        return  self.distinct_kmers - sum(self.histo[:coverage_cutoff])
+    def estimate_genome_size(self, coverage_cutoff):
+        """
+        The estimate genome size based on kmer content
+        :param coverage_cutoff: the count at which kmers are excluded
+        :return: the number of distinct kmers in the strain above cutoff
+        """
+        return  (np.sum(self.histo.values()[int(coverage_cutoff):]))
 
 
     def set_cutoff(self, cutoff):
@@ -77,22 +128,6 @@ class jf_object:
 
     def get_shared_count(self):
         return self.shared_count
-
-    ####DELETE#####
-    def percentage_of_kmers(self):
-        count = 1
-        sum = 0.0
-        for v in self.histo_total:
-            sum += v
-            print count, sum,  "{:.1f}%".format(sum / self.total_kmers * 100.0)
-            count += 1
-
-            if count > self.coverage:
-                return
-
-    def generate_coverage_histogram(self):
-        pass
-
 
     def __str__(self):
         return "Name:\t{0}\n".format(self.name) + \
@@ -106,6 +141,11 @@ class jf_object:
     def get_kmer_count(self, jellyfish_obj, break_point):
         counter = 0
         _arr = []
+
+        #add extra info
+        ardb = self.compare_to_ardb()
+
+
         for i in open(jellyfish_obj, "r"):
             #print i
             mer, count = i.strip().split("\t")
@@ -116,7 +156,22 @@ class jf_object:
             counter += 1
             if break_point is not None and counter >= break_point:
                 break
-        return (self.name, _arr)
+        return (self.name, _arr, ardb)
 
-    def compare_to_ardb(self, jfdb):
-        pass
+    def compare_to_ardb(self):
+        _dict = {}
+        for _file in os.listdir(self.ardb_dir):
+            gene = _file.split(".")[0]
+            for i in open(self.ardb_dir + _file, "r"):
+                mer, count = i.strip().split("\t")
+                mer = jellyfish.MerDNA(mer)
+                mer.canonicalize()
+                if  self.qf[mer] != 0:
+                    if gene in _dict:
+                        _dict[gene] += 1
+                    else:
+                        _dict.update({gene:1})
+        return _dict
+
+    def get_ardb_info(self, k):
+        return self.ardb_info_parsed[k]